f81fdc6598578f5a72369a96dbd8d5ff37df64d2
KaonashiMFG <dharmasenaakmalaji@outlook.com>
2024-11-29 21:17:58 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <>
+      {/* nav-bar */}
+      <div className="flex m-7 justify-between">
+        <p>Dharmasena</p>
+
+        <div className="bg-green-300 rounded-xl p-3 flex gap-16">
+          <span>Home</span>
+          <span>About</span>
+          <span>Contact</span>
+        </div>
+
+        <div></div>
+      </div>
+    </>
+  );
+}
